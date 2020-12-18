73dd4557c791daa0a67fa7c7b90c4bf78c6b25f4
George Rouse <george.rouse@gmail.com>
2020-12-18 09:33:10 +0000

    Update day_18.py

--- day_18.py
+++ day_18.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def evaluate_char_list(char_list, return_type='str'):
     result = 0.0
     for i, char in enumerate(char_list):
@@ -14,12 +17,40 @@ def evaluate_char_list(char_list, return_type='str'):
     else:
         return result
 
+
+def evaluate_char_list_v2(char_list, return_type='str'):
+    plus_stack = []
+    for i, char in enumerate(char_list):
+        if char == '+':
+            plus_stack.append((i, char))
+
+    if plus_stack:
+        while plus_stack:
+            for i, plus_value in enumerate(plus_stack):
+                left_val = int(char_list[plus_value[0]-1])
+                right_val = int(char_list[plus_value[0]+1])
+                result = f'{left_val + right_val}'
+                for index in range(plus_value[0]-1, plus_value[0]+2):
+                    char_list.pop(plus_value[0]-1)
+                char_list.insert(plus_value[0]-1, result)
+                plus_stack.pop(0)
+                plus_stack = []
+                for i, char in enumerate(char_list):
+                    if char == '+':
+                        plus_stack.append((i, char))
+                break
+
+    number_list = [int(x) for x in char_list if x != '*']
+    return_value = reduce(lambda x, y: x*y, number_list)
+    if return_type == 'str':
+        return str(return_value)
+    else:
+        return return_value
+
 if __name__ == '__main__':
     # Get the input data
     with open('inputs/day_18.txt') as f:
         file_data = f.read()
-        
-    # Format the input
     lines = [x.replace(' ', '') for x in file_data.split('\n')]
 
     # Part 1
@@ -27,7 +58,6 @@ if __name__ == '__main__':
     for line in lines:
         parenth_stack = []
         char_list = [x for x in line]
-        print(char_list)
         for i, char in enumerate(char_list):
             if char in ['(', ')']:
                 parenth_stack.append((i, char))
@@ -43,7 +73,6 @@ if __name__ == '__main__':
                         for index in range(first_parenth[0], second_parenth[0]+1):
                             char_list.pop(first_parenth[0])
                         char_list.insert(first_parenth[0], result)
-                        print(char_list)
                         parenth_stack.pop(i)
                         parenth_stack.pop(i)
                         break
@@ -58,4 +87,36 @@ if __name__ == '__main__':
         total_line_sum += line_result
     print(f'Answer to part 1: {total_line_sum}')
 
-    import pdb;pdb.set_trace()
+    # Part 2
+    total_line_sum = 0
+    for line in lines:
+        parenth_stack = []
+        char_list = [x for x in line]
+        for i, char in enumerate(char_list):
+            if char in ['(', ')']:
+                parenth_stack.append((i, char))
+
+        if parenth_stack:
+            while parenth_stack:
+                for i, parenth in enumerate(parenth_stack):
+                    first_parenth = parenth_stack[i]
+                    second_parenth = parenth_stack[i+1]
+                    if first_parenth[1] == '(' and second_parenth[1] == ')':
+                        in_parenth = char_list[first_parenth[0]+1:second_parenth[0]]
+                        result = evaluate_char_list_v2(in_parenth)
+                        for index in range(first_parenth[0], second_parenth[0]+1):
+                            char_list.pop(first_parenth[0])
+                        char_list.insert(first_parenth[0], result)
+                        parenth_stack.pop(i)
+                        parenth_stack.pop(i)
+                        break
+                if '(' not in char_list and ')' not in char_list:
+                    break
+                parenth_stack = []
+                for i, char in enumerate(char_list):
+                    if char in ['(', ')']:
+                        parenth_stack.append((i, char))
+
+        line_result = evaluate_char_list_v2(char_list, return_type='int')
+        total_line_sum += line_result
+    print(f'Answer to part 2: {total_line_sum}')
